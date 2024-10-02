1184c99225116016306a5addcd86dfea733878f0
端　祐太朗 <2211140031k@ed.fuk.kindai.ac.jp>
2024-10-02 09:14:51 +0000

    push to render.com

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+workers ENV.fetch("WEB_CONCURRECY"){2}
+preload_app!
+
