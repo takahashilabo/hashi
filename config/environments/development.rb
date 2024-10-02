0fd309515abf7d5f999e2dd77d3bf79db70972f8
端　祐太朗 <2211140031k@ed.fuk.kindai.ac.jp>
2024-10-02 10:42:44 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "08febb65f190463e82852f895ca2aedf.vfs.cloud9.us-east-1.amazonaws.com"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
